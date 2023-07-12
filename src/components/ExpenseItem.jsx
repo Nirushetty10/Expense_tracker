bc12c3f5bc236b3e22ec7e917bd8a457e613735e
Nirushetty10 <nirushetty582@gmail.com>
2023-07-12 18:43:05 +0530

    second

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from 'react';
+import "./ExpenseItem.css"
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+    let month = props.date.toLocaleString("en-US", {month : "long"});
+    let day = props.date.toLocaleString("en-US", {day : "2-digit"});
+    let year = props.date.getFullYear();
+    
   return (
-    <div>ExpenseItem</div>
+    <div className='expense-item'>
+        <div>
+            <div>{month}</div>
+            <div>{day}</div>
+            <div>{year}</div>
+        </div>
+        <div className='expense-item__description'> 
+            <h2 >{props.title}</h2>
+            <div className='expense-item__price'>${props.amount}</div>
+        </div>
+    </div>
   )
 }
 
